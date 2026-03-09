9086f3eee46970f9ce03ff9c7f293f74fa233a9a
Robpol86 <robpol86@gmail.com>
2026-03-09 14:47:34 +0900

    Better placeholder.
    Make is more obvious this is a mocked value.

--- jest.setup.mjs
+++ jest.setup.mjs
@@ -1,7 +1,7 @@
 import jest from "jest-mock";
 
 Object.assign(global, {
-    __FNAME_LINENO__: "mocked.mjs:123",
+    __FNAME_LINENO__: "__FNAME_LINENO__:0",
     _localStorage: { getItem: jest.fn(), setItem: jest.fn(), removeItem: jest.fn(), clear: jest.fn() },
     alert: jest.fn(),
 });
