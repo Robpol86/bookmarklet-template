d15408968b5f3811a7bbeb75b9b492fa5fa9d345
Robpol86 <robpol86@gmail.com>
2026-03-09 14:44:54 +0900

    Mocking __FNAME_LINENO__ in jest.
    This is handled in rollup but caused modules to be unimportable in unit
    tests due to the variable being undefined. Defining it with a mock
    value.
    
    Also sorting lines in the file for style.

--- jest.setup.mjs
+++ jest.setup.mjs
@@ -1,6 +1,7 @@
 import jest from "jest-mock";
 
 Object.assign(global, {
-    alert: jest.fn(),
+    __FNAME_LINENO__: "mocked.mjs:123",
     _localStorage: { getItem: jest.fn(), setItem: jest.fn(), removeItem: jest.fn(), clear: jest.fn() },
+    alert: jest.fn(),
 });
